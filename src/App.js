b351310a193b650fea1791ba78ded789734a251d
Nguyentuananh1102 <108406056+Nguyentuananh1102@users.noreply.github.com>
2022-12-28 23:47:42 +0700

    kiemtra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import ProductCart from './Component/ListCart/ProductCart'
+
+
+function App() {
+  return (
+    <>
+      <ProductCart/>
+    </>
+  )
+}
+
+export default App
